1d19185c5c0de24bf47b21a21fbd8436cf2e0a4c
MilOlivera <mil_olivera@hotmail.com>
2024-03-04 11:00:27 -0300

    responsive

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+
+    return(
+        <div>
+            <p>
+                <span>MILTON OLIVERA | COPYRIGHT 2023</span>
+            </p>
+        </div>
+    )
+}
+
+export default Footer; 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,18 @@
+
+const Form = () => {
+
+    return(
+        <>
+            <div className="contenedorNav">
+                
+                <div className="regresoHome">
+                    
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+
+export default Form;
